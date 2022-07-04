dbcf715840e4d24cd5d98b144e0a8ef406a3f7e5
benjouira <rabi3jouira@gmail.com>
2022-07-04 13:06:43 +0100

    sort a dict with lambda function

--- lambda.py
+++ lambda.py
@@ -2,13 +2,21 @@ people =[
     {"name":"rabii","city":"sfax"},
     {"name":"may","city":"tunis"},
     {"name":"catrina","city":"berlin"},
-    {"name":"anita","city":"LA"}
+    {"name":"anita","city":"canada"}
 ]
 
+#  first methode
 def fn(people):
     return people["name"]
     # return people.name  ma temchich haka
 
 people.sort(key=fn)
 
+# Lambda methode
+people.sort(key= lambda x : x["city"])
+
+
 print (people)
+
+
+
