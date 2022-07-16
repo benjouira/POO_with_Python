e841cd7d7fa668ab63d912830bfc2ec6bea8d9a7
Rabii <rabi3jouira@gmail.com>
2022-07-16 18:36:22 +0100

    Update Parrot.py

--- Parrot.py
+++ Parrot.py
@@ -19,3 +19,35 @@ print("Woo is also a {}".format(woo.__class__.species))
 # access the instance attributes
 print("{} is {} years old".format( blu.name, blu.age))
 print("{} is {} years old".format( woo.name, woo.age))
+
+/**************************************************************
+# parent class
+class Bird:
+    
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print("Bird")
+
+    def swim(self):
+        print("Swim faster")
+
+# child class
+class Penguin(Bird):
+
+    def __init__(self):
+        # call super() function
+        super().__init__()
+        print("Penguin is ready")
+
+    def whoisThis(self):
+        print("Penguin")
+
+    def run(self):
+        print("Run faster")
+
+peggy = Penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
